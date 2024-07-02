08140733389d1a3d2e63ad973dc7d1046ae7f72a
iVanChu1 <ivan.chudin@gmail.com>
2024-07-02 12:22:23 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,14 +1,14 @@
 THREADS = 1
-DELAY = (1, 2)
+DELAY = (10, 20)
 
 # Withdraw Params
 TICKER = "USDT"  # ETH, BTC ...
-AMOUNT = (1, 1)  # get random amount in range or if (2, 2) will send 2
+AMOUNT = (5, 10)  # get random amount in range or if (2, 2) will send 2
 CHAIN = "BSC"  # ERC20, TRC20 ...
 
 # COINEX AUTHENTICATION
-ACCESS_ID = ""
-SECRET_KEY = ""
+ACCESS_ID = "22FD58E775D2462FBBB68A462CF26079"
+SECRET_KEY = "22FD58E775D2462FBBB68A462CF26079"
 
 
 ########################################
